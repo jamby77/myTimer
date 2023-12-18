ef65c401c6c0c6e75ac32e6ee550bfe742915b60
Petar Djambazov <p.djambazov@jobs.bg>
2023-12-18 15:16:04 +0200

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by Petar Dzhambazov on 18.12.23.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
